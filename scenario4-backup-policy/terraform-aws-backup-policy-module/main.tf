5949738450e06d2b3bc4f545001e324ee275e8f8
Renzo Avila <renzo@avila.ws>
2025-04-17 18:14:44 +0200

    feat(scenario4): Add data sources for AWS Backup Policy module
    - Introduce data sources for AWS caller identity and current region in `main.tf`, enabling the module to retrieve necessary AWS context information for backup policy operations.

---
+++ scenario4-backup-policy/terraform-aws-backup-policy-module/main.tf
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# Main Resources for the AWS Backup Policy Module
+# ------------------------------------------------------------------------------
+
+# --- Data Sources ---
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
