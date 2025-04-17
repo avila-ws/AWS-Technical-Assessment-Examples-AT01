cb0ef3c58176a0f3c96b5e7e3bf07a69ad8b244f
Renzo Avila <renzo@avila.ws>
2025-04-17 19:00:00 +0200

    feat(scenario4): Add additional outputs for AWS Backup Policy module
    - Introduce new outputs for backup plan ID, primary backup vault ARN, primary backup vault name, and backup IAM role ARN, enhancing the visibility of the module's resources and improving usability for users.

--- scenario4-backup-policy/terraform-aws-backup-policy-module/outputs.tf
+++ scenario4-backup-policy/terraform-aws-backup-policy-module/outputs.tf
@@ -6,3 +6,23 @@ output "backup_plan_arn" {
   description = "The ARN of the created AWS Backup Plan."
   value       = aws_backup_plan.main.arn
 }
+
+output "backup_plan_id" {
+  description = "The ID of the created AWS Backup Plan."
+  value       = aws_backup_plan.main.id
+}
+
+output "primary_backup_vault_arn" {
+  description = "The ARN of the primary AWS Backup Vault created by the module."
+  value       = aws_backup_vault.primary.arn
+}
+
+output "primary_backup_vault_name" {
+  description = "The Name of the primary AWS Backup Vault created by the module."
+  value       = aws_backup_vault.primary.name
+}
+
+output "backup_iam_role_arn" {
+  description = "The ARN of the IAM role created for AWS Backup service."
+  value       = aws_iam_role.backup_role.arn
+}
