d554e0b665626e3023129814c00661a787cfe251
Renzo Avila <renzo@avila.ws>
2025-04-17 19:00:23 +0200

    feat(scenario4): Add additional outputs for IAM role and cross-account vault policy in AWS Backup Policy module
    - Introduce output for the IAM role name created for AWS Backup service, enhancing resource visibility.
    
    - Add conditional output for cross-account destination vault policy JSON, improving flexibility for cross-account backup configurations.

--- scenario4-backup-policy/terraform-aws-backup-policy-module/outputs.tf
+++ scenario4-backup-policy/terraform-aws-backup-policy-module/outputs.tf
@@ -26,3 +26,14 @@ output "backup_iam_role_arn" {
   description = "The ARN of the IAM role created for AWS Backup service."
   value       = aws_iam_role.backup_role.arn
 }
+
+output "backup_iam_role_name" {
+  description = "The Name of the IAM role created for AWS Backup service."
+  value       = aws_iam_role.backup_role.name
+}
+
+output "cross_account_destination_vault_policy_json" {
+  description = "[Conditional] JSON policy document that MUST be applied to the cross-account destination vault to allow copies from the source account. Only generated if cross-account copy is enabled."
+  # Generate policy only if cross_account_copy is enabled, otherwise output null or empty string
+  value       = var.enable_cross_account_copy ? data.aws_iam_policy_document.cross_account_vault_access[0].json : "Cross-account copy not enabled. No policy generated."
+}
