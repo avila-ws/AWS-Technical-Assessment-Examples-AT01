c8a93485eecd9c6aa04ff010e163dd2eb12f9167
Renzo Avila <renzo@avila.ws>
2025-04-17 18:40:14 +0200

    feat(scenario4): Add IAM role and policy attachment for AWS Backup Service
    - Introduce a new IAM role for AWS Backup, allowing it to manage backups on behalf of the user.
    
    - Attach the AWS Managed Policy for AWS Backup service role to the newly created IAM role, enhancing permissions for backup operations.

--- scenario4-backup-policy/terraform-aws-backup-policy-module/iam.tf
+++ scenario4-backup-policy/terraform-aws-backup-policy-module/iam.tf
@@ -18,3 +18,20 @@ data "aws_iam_policy_document" "backup_assume_role" {
     }
   }
 }
+
+# IAM Role for AWS Backup
+resource "aws_iam_role" "backup_role" {
+  name               = local.backup_role_name
+  description        = "IAM role allowing AWS Backup to manage backups on behalf of the user."
+  assume_role_policy = data.aws_iam_policy_document.backup_assume_role.json
+  tags = merge(var.tags, {
+    Name = local.backup_role_name
+  })
+}
+
+# Attach the AWS Managed Policy for AWS Backup service role
+resource "aws_iam_policy_attachment" "backup_policy_attachment" {
+  name       = "${local.backup_role_name}-attachment"
+  roles      = [aws_iam_role.backup_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
